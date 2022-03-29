3a98d5a8f703ee05d14d1c99a4c5a1033acdaae3
Gafarov Evgenii <evgeniy-gafarov@yandex.ru>
2022-03-29 08:49:59 +0300

    New changes

--- src/test/java/ru/netology/delivery/test/CardFormTest.java
+++ src/test/java/ru/netology/delivery/test/CardFormTest.java
@@ -2,6 +2,10 @@ package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.delivery.data.DataGenerator;
@@ -14,6 +18,16 @@ import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.Keys.BACK_SPACE;
 
 public class CardFormTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     void setUp() {
         Configuration.holdBrowserOpen = true;
